a53a615721ccaf04e25b6c3d237bc9ac375ea7df
Douglas Davis <ddavis@cern.ch>
2017-04-11 19:32:11 -0400

    setup script, move tree to hist

--- aidapy/hist.py
+++ aidapy/hist.py
@@ -22,3 +22,33 @@ def shift_overflow(hist):
     hist.SetBinError(nb,math.sqrt(err1*err1 + err2*err2))
     hist.SetBinContent(nb+1,0.0)
     hist.SetBinError(nb+1,0.0)
+
+def tree2hist(tree,hist_name,binning,var,cut,overflow=False):
+    """
+    A function to create a histogram using TTree::Draw()
+
+    Parameters
+    ----------
+    tree : The ROOT tree or chain
+    hist_name : the name-in-memory of the histogram to be created
+    binning : the binning of the histogram (nbins,xmin,xmax)
+    var : string for the variable in the tree to histogram
+    cut : the selection string
+
+    Returns
+    -------
+    ROOT.TH1F
+        The ROOT histogram created
+
+    """
+    ROOT.gROOT.SetBatch(True)
+    ROOT.TH1.SetDefaultSumw2()
+    if not isinstance(tree,ROOT.TTree):
+        raise TypeError("Must be ROOT TTree or TChain")
+
+    bin_str  = '('+str(binning[0])+','+str(binning[1])+','+str(binning[2])+')'
+    tree.Draw(var+'>>'+hist_name+bin_str,cut)
+    hist = ROOT.gDirectory.Get(hist_name)
+    if overflow:
+        shift_overflow(hist)
+    return hist
--- lxsetup.sh
+++ lxsetup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-lsetup 'sft releases/LCG_87/numpy/1.11.0'
-lsetup 'sft releases/LCG_87/scipy/0.15.1'
-lsetup 'sft releases/LCG_87/matplotlib/1.5.1'
+if [ -z "$ROOTCOREBIN" ]; then
+    echo "Warning: ROOTCOREBIN env var not set"
+    echo "Setting up a standalone ROOT release"
+    echo "If this is not your desired setup, start over with your RootCore area set up."
+    lsetup root
+fi
+
+lsetup 'sft releases/LCG_88/numpy/1.11.0'
+lsetup 'sft releases/LCG_88/scipy/0.18.1'
+lsetup 'sft releases/LCG_88/matplotlib/1.5.1'
