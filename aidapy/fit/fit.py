477c3091ee55ff8ebbb5d5c8cdfd4338620ec019
Douglas Davis <ddavis@cern.ch>
2017-05-15 19:12:48 +0200

    add dummy fit playground

---
+++ aidapy/fit/fit.py
@@ -0,0 +1,52 @@
+import ROOT
+
+meas = ROOT.RooStats.HistFactory.Measurement("meas","meas")
+meas.SetOutputFilePrefix("./results/aidaFit")
+meas.SetExportOnly(False)
+meas.SetPOI("mu_TTBAR")
+meas.SetPOI("mu_ZTAUTAU")
+meas.SetPOI("mu_WT")
+#meas.AddConstanstParam("Lumi")
+#meas.AddConstanstParam("alpha_syst1")
+#meas.AddConstantParam("alpha_ttbar_AR")
+#meas.AddConstantParam("alpha_Wt_AR")
+#meas.AddConstantParam("alpha_Ztautau_JFR")
+
+meas.SetLumi(1.0)
+meas.SetLumiRelErr(0.03)
+
+chan = ROOT.RooStats.HistFactory.Channel("1pj")
+chan.SetData("Data_met_1pj","aida_histograms.root")
+
+ttbar = ROOT.RooStats.HistFactory.Sample("ttbar_met_1pj","ttbar_FULL_main_nominal_met_1pj","aida_histograms.root")
+ttbar.AddHistoSys("ttbar_AR","ttbar_FULL_sysARdown_nominal_met_1pj","aida_histograms.root","","ttbar_FULL_sysARup_nominal_met_1pj","aida_histograms.root","")
+ttbar.AddNormFactor("mu_TTBAR",1,0,3)
+chan.AddSample(ttbar)
+
+Wt = ROOT.RooStats.HistFactory.Sample("Wt_met_1pj","Wt_FULL_main_nominal_met_1pj","aida_histograms.root")
+Wt.AddHistoSys("Wt_AR","Wt_FULL_sysARdown_nominal_met_1pj","aida_histograms.root","","Wt_FULL_sysARup_nominal_met_1pj","aida_histograms.root","")
+Wt.AddNormFactor("mu_WT",1,0,3)
+chan.AddSample(Wt)
+
+Ztautau = ROOT.RooStats.HistFactory.Sample("Ztautau_met_1pj","Ztautau_FULL_main_nominal_met_1pj","aida_histograms.root")
+Ztautau.AddHistoSys("Ztautau_JFR",
+                    "Ztautau_FULL_main_JET_21NP_JET_Flavor_Response__1down_met_1pj","aida_histograms.root","",
+                    "Ztautau_FULL_main_JET_21NP_JET_Flavor_Response__1up_met_1pj",  "aida_histograms.root","")
+Ztautau.AddNormFactor("mu_ZTAUTAU",1,0,3)
+chan.AddSample(Ztautau)
+WW = ROOT.RooStats.HistFactory.Sample("WW_met_1pj","WW_FULL_main_nominal_met_1pj","aida_histograms.root")
+chan.AddSample(WW)
+Diboson = ROOT.RooStats.HistFactory.Sample("Diboson_met_1pj","Diboson_FULL_main_nominal_met_1pj","aida_histograms.root")
+chan.AddSample(Diboson)
+Fakes = ROOT.RooStats.HistFactory.Sample("Fakes_met_1pj","Fakes_FULL_main_nominal_met_1pj","aida_histograms.root")
+chan.AddSample(Fakes)
+RareSM = ROOT.RooStats.HistFactory.Sample("RareSM_met_1pj","RareSM_FULL_main_nominal_met_1pj","aida_histograms.root")
+chan.AddSample(RareSM)
+
+meas.AddChannel(chan)
+meas.CollectHistograms()
+meas.PrintTree()
+#meas.PrintXML("xmlFromPy", meas.GetOutputFilePrefix());
+ROOT.RooStats.HistFactory.MakeModelAndMeasurementFast(meas)
+
+pass
