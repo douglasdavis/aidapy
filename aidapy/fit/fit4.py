3e0568b5a79d23be6e6bd21f094894a011b10335
Douglas Davis <ddavis@cern.ch>
2017-06-09 17:43:12 +0200

    fit play

--- aidapy/fit/fit4.py
+++ aidapy/fit/fit4.py
@@ -1,76 +1,69 @@
 from __future__ import print_function
-
 import aidapy.meta as am
-
 import ROOT
 HF = ROOT.RooStats.HistFactory
 RF = ROOT.RooFit
 
 rootFile = "/Users/ddavis/Software/aidapy/aida_histograms.root"
+
 meas = HF.Measurement("measurement","measurement")
-meas.SetOutputFilePrefix("./out4/plss")
+meas.SetOutputFilePrefix("./out5/plss")
 meas.AddPOI("mu_TTBAR")
 meas.AddPOI("mu_WW")
 meas.AddPOI("mu_ZTAUTAU")
 meas.AddConstantParam("Lumi")
-meas.AddConstantParam("alpha_syst1")
 
 meas.SetLumi(1.0)
-meas.SetLumiRelErr(0.10)
+meas.SetLumiRelErr(0.04)
 meas.SetExportOnly(False)
-channel = HF.Channel("channel")
-channel.SetData("Data_met_1pj",rootFile)
 
-samples = {}
-samples["ttbar"]   = HF.Sample("ttbar",  "ttbar_FULL_main_nominal_met_1pj",  rootFile,"")
-samples["WW"]      = HF.Sample("WW",     "WW_FULL_main_nominal_met_1pj",     rootFile,"")
-samples["Ztautau"] = HF.Sample("Ztautau","Ztautau_FULL_main_nominal_met_1pj",rootFile,"")
-for sample in samples:
-    samples[sample].AddNormFactor("mu_"+sample.upper(),1.0,0.1,2.5)
-#samples["ttbar"].AddNormFactor("mu_TTBAR",1.0,0.1,2.5)
-#samples["WW"].AddNormFactor("mu_WW",1.0,0.1,2.5)
-#samples["Ztautau"].AddNormFactor("mu_ZTAUTAU",1.0,0.1,2.5)
+def setup_channel(name):
+    chan = HF.Channel(name)
+    chan.SetData("Data_"+name,rootFile)
+    samples = {}
+    samples["ttbar"]   = HF.Sample("ttbar",  "ttbar_FULL_main_nominal_"+name,  rootFile,"")
+    samples["WW"]      = HF.Sample("WW",     "WW_FULL_main_nominal_"+name,     rootFile,"")
+    samples["Ztautau"] = HF.Sample("Ztautau","Ztautau_FULL_main_nominal_"+name,rootFile,"")
+    for sample in samples:
+        samples[sample].AddNormFactor("mu_"+sample.upper(),1.0,0.1,2.5)
+    samples["Wt"]      = HF.Sample("Wt",     "Wt_FULL_main_nominal_"+name,     rootFile,"")
+    samples["Diboson"] = HF.Sample("Diboson","Diboson_FULL_main_nominal_"+name,rootFile,"")
+    samples["Fakes"]   = HF.Sample("Fakes",  "Fakes_FULL_main_nominal_"+name,  rootFile,"")
+    samples["RareSM"]  = HF.Sample("RareSM", "RareSM_FULL_main_nominal_"+name, rootFile,"")
 
-samples["ttbar"].AddHistoSys("ttbar_AR",
-                             "ttbar_FULL_sysARdown_nominal_met_1pj",rootFile,"",
-                             "ttbar_FULL_sysARup_nominal_met_1pj",  rootFile,"")
+    for treesys in am._systematic_ud_prefixes:
+        downsfx = "__1down"
+        upsfx   = "__1up"
+        if 'MET' in treesys:
+            downsfx = "Down"
+            upsfx   = "Up"
+        for iproc in ["ttbar","WW","Ztautau","Wt","Diboson","Fakes","RareSM"]:
+            samples[iproc].AddHistoSys(treesys,
+                                       iproc+"_FULL_main_"+treesys+downsfx+"_"+name,rootFile,"",
+                                       iproc+"_FULL_main_"+treesys+upsfx+  "_"+name,rootFile,"")
 
-for treesys in am._systematic_ud_prefixes:
-    downsfx = "__1down"
-    upsfx   = "__1up"
-    if 'MET' in treesys:
-        downsfx = "Down"
-        upsfx   = "Up"
-    for iproc in ["ttbar","WW","Ztautau"]:
-        samples[iproc].AddHistoSys(treesys,
-                                   iproc+"_FULL_main_"+treesys+downsfx+"_met_1pj",rootFile,"",
-                                   iproc+"_FULL_main_"+treesys+upsfx+  "_met_1pj",rootFile,"")
+    samples["ttbar"].AddHistoSys("ttbar_AR",
+                                 "ttbar_FULL_sysARdown_nominal_"+name,rootFile,"",
+                                 "ttbar_FULL_sysARup_nominal_"+name,  rootFile,"")
 
-channel.AddSample(samples["ttbar"])
-channel.AddSample(samples["WW"])
-channel.AddSample(samples["Ztautau"])
+    chan.AddSample(samples["ttbar"])
+    chan.AddSample(samples["WW"])
+    chan.AddSample(samples["Ztautau"])
+    chan.AddSample(samples["Wt"])
+    chan.AddSample(samples["Diboson"])
+    chan.AddSample(samples["Fakes"])
+    chan.AddSample(samples["RareSM"])
 
-#samples["ttbar"].AddOverallSys("syst1",0.9,1.5)
-#samples["WW"].AddOverallSys("syst2",0.45,1.25)
-#samples["Ztautau"].AddOverallSys("syst3",0.15,1.95)
+    return chan
 
-bkg0 = HF.Sample("Wt",     "Wt_FULL_main_nominal_met_1pj",     rootFile,"")
-bkg1 = HF.Sample("Diboson","Diboson_FULL_main_nominal_met_1pj",rootFile,"")
-bkg2 = HF.Sample("Fakes",  "Fakes_FULL_main_nominal_met_1pj",  rootFile,"")
-bkg3 = HF.Sample("RareSM", "RareSM_FULL_main_nominal_met_1pj", rootFile,"")
+channel_names = ["met_0j","met_1pj"]
+for chan in channel_names:
+    c = setup_channel(chan)
+    meas.AddChannel(c)
 
-bkg0.ActivateStatError()
-bkg1.ActivateStatError()
-bkg2.ActivateStatError()
-bkg3.ActivateStatError()
-channel.AddSample(bkg0)
-channel.AddSample(bkg1)
-channel.AddSample(bkg2)
-channel.AddSample(bkg3)
-
-meas.AddChannel(channel)
+#meas.AddChannel(channel)
 meas.CollectHistograms()
 meas.PrintTree()
-#print(RF.NumCPU(4))
-#ws = HF.MakeModelAndMeasurementFast(meas)
+RF.NumCPU(4)
+ws = HF.MakeModelAndMeasurementFast(meas)
 exit(0)
