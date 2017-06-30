9fc87030705a8572c9cc47e2ef5a796aa3601fd8
Douglas Davis <ddavis@cern.ch>
2017-06-30 14:52:21 +0200

    add one sided tree systematics

--- data/tthfitter/sys_dumper.py
+++ data/tthfitter/sys_dumper.py
@@ -14,3 +14,13 @@ for s in aidapy.meta._systematic_ud_prefixes:
     print('  HistoNameSufDown: "_'+s+down)
     print('  Regions: met_1pj,met_0j')
     print('')
+
+for s in aidapy.meta._systematic_singles:
+    print('Systematic: "'+s+'"')
+    print('  Title: "'+s+'"')
+    print('  Type: HISTO')
+    print('  Samples: ttbar,Wt,WW,Ztautau,Diboson,Fakes,RareSM')
+    print('  HistoNameSufUp: "_'+s+'"')
+    print('  Symmetrisation: ONESIDED')
+    print('  Regions: met_1pj,met_0j')
+    print('')
